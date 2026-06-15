4be1d5a4f80dd5be16ca4819dc268a20d9185a79
Priyam <priyamsaini241178@gmail.com>
2026-06-15 21:42:06 +0530

    fixed que 10

--- Assignment_3/Question_10.py
+++ Assignment_3/Question_10.py
@@ -1,26 +1,29 @@
-# Create a class named LibraryBook with attributes:  
-# ● book_name  
-# ● author  
-# ● price              
-# Create a method display_book_info() that prints all book details. 
+# Create a class named LibraryBook with attributes:
+# ● book_name
+# ● author
+# ● price
+# Create a method display_book_info() that prints all book details.
 # Create at least three book objects and display their information
 
-#Solution -->
+# Solution -->
 
-class LibraryBook :
-    def __init__(self,book_name,author,price):
+
+class LibraryBook:
+    def __init__(self, book_name, author, price):
         self.book_name = book_name
         self.author = author
         self.price = price
+
     def display_book_info(self):
         print(f"Book name : {self.book_name}")
         print(f"Book author : {self.author}")
         print(f"Book price : {self.price}")
         print("-------")
 
-l1 = LibraryBook("LOSING MY VIRGINITY","RICHARD BRANSON",599)
-l2 = LibraryBook("THE MONK WHO SOLD HIS FERRARI","ROBIN SHARMA",225)
-l3 = LibraryBook("THE SECOND SEX","DE BEAUVOIR",999)
+
+l1 = LibraryBook("LOSING MY VIRGINITY", "RICHARD BRANSON", 599)
+l2 = LibraryBook("THE MONK WHO SOLD HIS FERRARI", "ROBIN SHARMA", 225)
+l3 = LibraryBook("THE SECOND SEX", "DE BEAUVOIR", 999)
 l1.display_book_info()
 l2.display_book_info()
 l3.display_book_info()
